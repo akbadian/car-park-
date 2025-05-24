fc7ee7a41839011eca3445d11767fc079ed21dbb
akbadian <akbadian09@gmail.com>
2025-05-24 19:58:38 +0100

    new updates

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,7 +4,7 @@ const user = require('../models/User');
 const { validationResult } = require('express-validator');
 const User = require('../models/User');
 require('dotenv').config();
-JWT_SECRET = process.env.JWT_SECRET
+const JWT_SECRET = process.env.JWT_SECRET
 
 // Register a new user
 exports.register = async (req, res) => {
@@ -31,28 +31,33 @@ exports.register = async (req, res) => {
 
 // Login a user
 exports.login = async (req, res) => {
-    const {email, password} = req.body;
-    const user = await User.findOne({email});
-    if (!user) {
-        return res.status(404).json({message : 'User not found !'});
-    }
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-        return res.status(400).json({message : 'Invalid credentials !'});
-    }
-    const token = jwt.sign({
-        id : user._id,
-        email : user.email
-        },
-        JWT_SECRET,
-        { expiresIn : '1h'}
-    );
-    res.status(200).json({
-        message : 'Login Successful',
-        token,
-        user : {
-            id : user._id, 
-            email : user.email
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        if (!user) {
+            return res.status(404).json({message : 'User not found !'});
         }
-    });
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({message : 'Invalid credentials !'});
+        }
+        const token = jwt.sign({
+            id : user._id,
+            email : user.email
+            },
+            JWT_SECRET,
+            { expiresIn : '1h'}
+        );
+        res.status(200).json({
+            message : 'Login Successful',
+            token,
+            user : {
+                id : user._id, 
+                email : user.email
+            }
+        });
+     } catch (error) {
+            console.error('Login error:', error);
+            res.status(500).json({ message: 'Internal server error', error: error.message });
+        }
 }
